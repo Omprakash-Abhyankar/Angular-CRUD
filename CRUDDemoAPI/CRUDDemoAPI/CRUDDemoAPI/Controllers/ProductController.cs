ee7afbf243f84664632d78e292da484a9bf87acf
Omprakash-ZWS <omprakash.abhyankar@zobiwebsolutions.com>
2023-10-17 10:19:22 +0530

    first commit

---
+++ CRUDDemoAPI/CRUDDemoAPI/CRUDDemoAPI/Controllers/ProductController.cs
@@ -0,0 +1,87 @@
+﻿using CRUDDemoAPI.Context;
+using CRUDDemoAPI.Model;
+using CRUDDemoAPI.Model.Viewmodel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDDemoAPI.Controllers
+{
+
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: ProductController
+        [HttpPost("CreateProduct")]
+        public async Task<IActionResult> CreateProduct(Products products)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+          products.ProductId = new Guid();
+            //_context.Products.Add(products);
+            _context.products.Add(products);
+            await _context.SaveChangesAsync();
+            return Ok("Product Added Successfully");
+        }
+
+        [HttpGet("GetProduct")]
+        public async Task<IEnumerable<Products>> GetProduct()
+        {
+            return await _context.products.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(Guid id)
+        {
+            if (id.ToString().Length == 0)
+                return BadRequest();
+            var product = await _context.products.FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+                return NotFound();
+            return Ok(product);
+        }
+        [HttpPut("UpdateProducts")]
+        public async Task<IActionResult> UpdateProducts(Products products)
+        {
+            if (products == null || products.ProductId.ToString().Length == 0)
+                return BadRequest();
+
+            var product = await _context.products.FindAsync(products.ProductId);
+            if (product == null)
+                return NotFound();
+            product.productName = products.productName;
+            product.category = products.category;
+            product.freshness = products.freshness;
+            product.price = products.price;
+            product.comment = products.comment;
+            product.date = products.date;
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpPatch("UpdateProduct")]
+        public async Task<IActionResult> UpdateProduct()
+        {
+            return View();
+        }
+        [HttpDelete("DeleteProduct")]
+        public async Task<IActionResult> DeleteProduct(Guid id)
+        {
+            if (id.ToString().Length == 0)
+                return BadRequest();
+            var product = await _context.products.FindAsync(id);
+            if (product == null)
+                return NotFound();
+            _context.products.Remove(product);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ CRUDDemoAPI/CRUDDemoAPI/CRUDDemoAPI/Controllers/ProductsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CRUDDemoAPI.Context;
+using CRUDDemoAPI.Model;
+
+namespace CRUDDemoAPI.Controllers
+{
+   
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+              return _context.products != null ? 
+                          View(await _context.products.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.products'  is null.");
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.products == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.products
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,productName,category,freshness,price,comment,date")] Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                products.ProductId = Guid.NewGuid();
+                _context.Add(products);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(products);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.products == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.products.FindAsync(id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+            return View(products);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("ProductId,productName,category,freshness,price,comment,date")] Products products)
+        {
+            if (id != products.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(products);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductsExists(products.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(products);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _context.products == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.products
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_context.products == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.products'  is null.");
+            }
+            var products = await _context.products.FindAsync(id);
+            if (products != null)
+            {
+                _context.products.Remove(products);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductsExists(Guid id)
+        {
+          return (_context.products?.Any(e => e.ProductId == id)).GetValueOrDefault();
+        }
+    }
+}
